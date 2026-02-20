0f9458aa2a36235c52e22cfc82b3a39e414b0db3
Kavya Govindarajan <kavya.g@ibm.com>
2026-02-20 11:30:07 +0530

    Add API for memory_allocated

--- torch_spyre/csrc/module.cpp
+++ torch_spyre/csrc/module.cpp
@@ -268,6 +268,7 @@ PYBIND11_MODULE(_C, m) {
   m.def("to_with_layout", &spyre::to_with_layout);
   m.def("empty_with_layout", &spyre::py_empty_with_layout);
   m.def("as_strided_with_layout", &spyre::as_strided_with_layout);
+  m.def("memory_allocated", &spyre::get_memory_allocated);
   m.def("spyre_reinterpret_tensor", &spyre::spyre_reinterpret_tensor);
 
   py::enum_<DataFormats>(m, "DataFormats")
